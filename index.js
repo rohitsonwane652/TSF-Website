f03c78255d190fcdb2344492e6e1fc5804a7e722
Rohit <rohitsonwane652@gmail.com>
2021-08-16 20:29:37 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 80;
+
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // For serving static files
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {}
+    res.status(200).render('index.pug', params);
+})
+
+
+app.get('/contact', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {}
+    res.status(200).render('contact.pug', params);
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
